aa8c64aa72f417ec236c09feb8ad72e938351655
tysonwells <tysonwells370@gmail.com>
2021-11-21 11:16:50 -0600

    Define about view

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-  return HttpResponse('Hello Tool Collector')
+  return HttpResponse('Hello, Tool Collector')
+
+def about(request):
+  return HttpResponse('About the Tools Collector')
--- toolscollector/settings.py
+++ toolscollector/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'toolscollector.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'toolscollector',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': 'toolscollector',
+	}
 }
 
 
