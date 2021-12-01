068fbe8f07f646c1582a05a08274cb558f42ca24
tysonwells <tysonwells370@gmail.com>
2021-11-30 21:09:52 -0600

    remove secret key env

--- toolscollector/settings.py
+++ toolscollector/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-*4f_djt#w-^wi7m&+p1skrp60ej@&pghlu%=zi7ief3xsf(jdz'
-SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True' 
